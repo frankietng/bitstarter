fbffd6ec1f365a8bf1d1324983a12aaa9b676c5d
FrankieTNG <frankietng@web.de>
2013-07-11 10:54:43 +0000

    Lesen aus index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response)
   var buf;
   buf=fs.readFileSync("index.html");
 
-  response.send(buf.toString);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
